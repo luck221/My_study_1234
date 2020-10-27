d982ec23d95f31f86c632c5050d3853c29a488fa
Apexfall <60819124+luck221@users.noreply.github.com>
2020-10-27 13:33:44 +0400

    Delete simple_menu_item.py

--- Menu/simple_menu_item.py
+++
@@ -1,8 +0,0 @@
-from .command import Command
-class SimpleMenuItem():
-    def __init__(self:SimpleMenuItem, commmand:Command) -> None:
-        self.__command = commmand 
-    def SimpleMenuItemMethod(number:int, title:str, command:Command):
-        pass
-    def execute(self:SimpleMenuItem):
-        pass
